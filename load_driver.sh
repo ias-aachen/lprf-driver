6540cbca56c49ba3c4f889645134bde5ba9c0f3e
jaribro <jan.richter-brockmann@rwth-aachen.de>
2016-07-19 14:59:30 +0200

    more dynamic script

--- load_driver.sh
+++ load_driver.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
+if ifconfig | grep "wpan0" ; then
+	echo "wpan0 is up and will be set down..."
+	ip link set wpan0 down
+else
+	echo "wpan0 is down"
+fi
+
+# check for loaded regmap module
+if lsmod | grep "regmap_spi" &> /dev/null ; then
+	echo "regmap_spi is loaded"
+else
+	echo "regmap_spi will be loaded..."
+	insmod /home/pi/kernel/linux/drivers/base/regmap/regmap-spi.ko
+fi
+
+# at86rf230 can't be loaded if we want to use the lprf driver
 if lsmod | grep "at86rf230" &> /dev/null ; then
-	echo "at86rf230 is loaded and will be unloaded now"
+	echo "at86rf230 is loaded and will be unloaded now..."
 	rmmod at86rf230
 else
 	echo "at86rf230 is not loaded"
 fi
 
-dmesg -c
+# clean up the dmesg
+echo "clean up the dmesg..."
+dmesg -c &> /dev/null
 
 if lsmod | grep "lprf_tx" &> /dev/null ; then
 	echo "lprf module is currently loaded:"
@@ -23,11 +41,14 @@ insmod lprf_tx.ko
 
 cat /proc/modules
 
+# create the device node for the char driver
 echo "create device node..."
-mknod /dev/lprf c 243 0
+major=$(awk "\$2==\"lprf\" {print \$1}" /proc/devices)
+rm -f /dev/lprf
+mknod /dev/lprf c $major 0
 
 echo "setup wpan device..."
-/home/pi/ieee802154/setup_wpan.sh
+ieee802154/setup_wpan.sh
 
 echo "start userspace client..."
-/home/pi/ieee802154/client_udp_802154
+ieee802154/client_udp_802154
